1b625b42d8c1488039be1d8f35472ef6249e445d
Raghav77Agrawal <raghavagrawal730@gmail.com>
2025-07-17 15:37:54 +0530

    'moremorechanges;

--- backend/index.js
+++ backend/index.js
@@ -12,7 +12,7 @@ const cors = require('cors');
 const server = http.createServer(app);
 const io = new Server(server, {
     cors: {
-        origin: "https://frontendchat-lemon.vercel.app/",
+        origin: process.env.frontendurl,
         methods: ['GET', 'POST'],
         credentials: true
     }
@@ -36,7 +36,7 @@ app.use(express.json());
 app.use(cookieparser());
 app.use(express.urlencoded({ extended: false }))
 app.use(cors({
-    origin: "https://frontendchat-lemon.vercel.app/",
+    origin: process.env.frontendurl,
     methods: ['GET', 'POST'],
     credentials: true, //for cookies authentication
     allowedHeaders: ['Content-Type', 'Authorization'],
